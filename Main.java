21de64a813c15ed533042e1e06d71f3ea03abc8a
Nikhil Borkar <140334641+UnicornNikhil2211@users.noreply.github.com>
2024-11-06 00:15:52 +0530

    Spelling Corrections

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println("I am learning Jenkin with Java very successfully. It has been greate experiance for me. Wow!!!");
+        System.out.println("I am learning Jenkins with Java very successfully. It has been grate experience for me. Wow!!!");
         System.out.println(new Date());
 
         System.out.println("It is an initial commit.");
