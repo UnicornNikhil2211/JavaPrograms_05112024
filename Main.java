57ff2e9716b0039916c5405130b554a70f993f1f
Nikhil Borkar <140334641+UnicornNikhil2211@users.noreply.github.com>
2024-11-05 12:17:40 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("I am learning Jenkin with Java very successfully. It has been greate experiance for me. Wow!!!");
+        System.out.println(new Date());
+    }
+}
